9fe2acb5ad761ce77c158a0a6865eb3629ac32ed
erwanLeGuen <erwan.leguen2@free.fr>
2016-09-27 16:41:38 +0200

    Version du Mardi 27 Sept

--- AccesDonnees.php
+++ AccesDonnees.php
@@ -6,15 +6,13 @@
  * 
  * @author Erwan
  * @copyright Estran
- * @version 4.5.1 Vendredi 23 Septembre 2016
+ * @version 4.6 Mardi 27 Septembre 2016
  *
- * Implementation PDO des fonctions :
- *    + compteSQL
- *    + tableSQL
- *    + compteSQL
- *    + champSQL
- *    + versionSQL
- *    
+ * Implementation de PDO terminee
+ *  - nomChamp OK
+ *  - afficheRequete OK
+ *  - dump OK
+ *  - TODO faire la restaure
  *    
  */
 
@@ -22,7 +20,7 @@
 ///////////// CONFIGURATION DE L'ACCES AUX DONNEES ////////////////////
 
 // nom du moteur d'accès à la base : mysql - mysqli - pdo
-$modeacces = "mysql";
+$modeacces = "pdo";
 
 // enregistrement des logs de connexion : true - false
 $logcnx = TRUE;
@@ -340,7 +338,6 @@ function compteSQL($sql) {
 	global $modeacces, $connexion;
 	
 	if ($modeacces=="pdo") {	
-		//utilisation d'une requete preparee
 		$repueteP=$connexion->prepare($sql);
 		$repueteP->execute();
 		$num_rows = $repueteP->rowCount();	
@@ -450,16 +447,13 @@ function typeChamp($sql, $field_offset) {
 
 	$result = executeSQL($sql);
 	
-	if ($modeacces=="pdo") {
-		
+	if ($modeacces=="pdo") {	
 		$posfrom = strpos(strtolower($sql), "from");
 		$newsql = substr($sql, $posfrom+5, strlen($sql)-5-$posfrom);
 		$nomtables = explode(',',$newsql);
 		$nomtable = trim($nomtables[0]);
-		//echo "$nomtable<br />";
 		$recordset = $connexion->query("SHOW COLUMNS FROM $nomtable");
 		$fields = $recordset->fetchAll(PDO::FETCH_ASSOC);
-		//var_dump($fields);
 		$letype = ($fields[$field_offset]["Type"]);
 		
 		if (stristr($letype,'varchar')!=FALSE) {
@@ -503,21 +497,20 @@ function nomChamp($sql, $field_offset) {
 	global $modeacces, $connexion, $mysql_data_type_hash;
 
 
-
 	/* getColumnMeta est EXPERIMENTALE. Cela signifie que le comportement de cette fonction, son nom et,
 	 * concrètement, TOUT ce qui est documenté ici peut changer dans un futur proche, SANS PREAVIS ! 
 	 * Soyez-en conscient, et utilisez cette fonction à vos risques et périls.
+	 * 
+	 *    $select = $connexion->query($sql);
+	 *	  $meta = $select->getColumnMeta($field_offset);
+	 *	  return ($meta["name"]);
 	 */
+	
 	if ($modeacces=="pdo") {
-		//utilisation d'une requete preparee
 		$requeteP=$connexion->prepare($sql);
 		$requeteP->execute();
-		$fields = $requeteP->fetch(PDO::FETCH_ASSOC);
-		var_dump($fields);
-		/*$select = $connexion->query($sql);
-		  $meta = $select->getColumnMeta($field_offset);
-		  return ($meta["name"]);
-		 */
+		$fields = $requeteP->fetch(PDO::FETCH_BOTH);
+		return (array_keys($fields)[$field_offset*2]);
 	}
 
 	if ($modeacces=="mysql") {
@@ -559,6 +552,316 @@ function versionMYSQL() {
 	}
 
 }
+
+
+
+/**
+ *
+ *Retourne le nom de la base courante
+ *
+ *
+ * @return Retourne une chaîne de caractères représentant le nom de la base de données
+ *         auquel l'extension  est connectée (représenté par le paramètre $connexion).
+ */
+function nomBase() {
+	
+	global $modeacces, $connexion;
+	
+	$sql = "SELECT DATABASE()";
+	return champSQL($sql);
+}
+
+
+
+/**
+ *
+ *Affiche sous forme d'un tableau le résultat d'une requette SQL
+ * @param sql string
+ *  <p>Requete SQL.</p>
+ *
+ *
+ */
+function afficheRequeteSQL($sql) {
+	
+	$results = tableSQL($sql);
+	
+	$nbchamps = nombreChamp($sql);
+	
+	echo "<table border=1 >";
+	echo "   <caption>$sql</caption>
+			 <tr>";
+				for ($i=0;$i<$nbchamps;$i++) {
+					echo "<th >".nomChamp($sql,$i)."</th>";
+				}
+	echo "   </tr>";
+	     
+	foreach ($results as $ligne) {
+		echo "<tr>";
+		//on extrait chaque valeur de la ligne courante
+		for ($i=0;$i<$nbchamps;$i++) {
+			echo "<td>".$ligne[$i]."</td>";
+		}
+		echo "</tr>";
+    }
+   
+    echo "</table>"; 
+}
+
+
+
+/**
+ *
+ *Sauvegarde (DUMP) dans un fichier texte la base de donnees courante.
+ * @param mode string
+ *  <p>S pour la structure de la base de donnees
+ *     D pour les donnees de la base
+ *     A pour la structure et les donnees de la base</p>
+ * @param repertoire string
+ *  <p>Nom du repertoire dans lequel le fichier dump sera sauvagarde
+ *     si NULL on sauvegarde dans le repertoire courant</p>
+ * @param nomfichier string
+ *  <p>Nom du fichier pour le dump (ajout de l'extension .slq)
+ *     si NULL on utilise 'nomDeLaBase'.sql </p>
+ *
+ */
+function export($mode,$repertoire,$nomfichier) {
+	
+	global $modeacces, $connexion;
+		
+	$dbname=nomBase();
+	$jour = array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+	$mois = array("","Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
+	
+	$entete  = "-- ----------------------\n";
+	$entete .= "-- dump de la base ".$dbname." du ".$jour[date("w")]." ".date("d")." ".$mois[date("n")].date(" Y \à H:i:s")."\n";
+	$entete .= "-- ----------------------\n\n\n";
+	
+	$creations ="";
+	$insertions="\n\n";
+	
+	$sql = "show tables";
+	$results = tableSQL($sql);
+	
+	
+	foreach ($results as $ligne) {
+		//on extrait chaque valeur de la ligne courante
+		$table = $ligne[0];
+	
+	
+		// structure de la table
+		$creations .= "-- -----------------------------\n";
+		$creations .= "-- Structure de la table ".$table."\n";
+		$creations .= "-- -----------------------------\n";
+	
+		$sql1="show create table ".$table;
+		$results1 = tableSQL($sql1);
+	
+		foreach ($results1 as $ligne1) {
+			//on extrait chaque valeur de la ligne courante
+			$requete = $ligne1[1];
+	
+			//on affiche la requete
+			$creations .= $requete.";\n\n";
+		}
+	
+	
+		// donnees de la table
+		$sql2 = "SELECT * FROM ".$table;
+		$results2 = tableSQL($sql2);
+	
+		$insertions .= "-- -----------------------------\n";
+		$insertions .= "-- Contenu de la table ".$table."\n";
+		$insertions .= "-- -----------------------------\n";
+	
+		foreach ($results2 as $ligne2) {
+	
+			$nombredechamps = nombrechamp($sql2);
+				
+			$insertions .= "INSERT INTO ".$table." VALUES(";
+	
+			for($i=0; $i <$nombredechamps; $i++) {
+					
+				$letypeduchamp = typechamp($sql2,$i);
+				$lavaleurduchamp =  $ligne2[$i];
+					
+				if($i != 0)
+					$insertions .=  ", ";
+						
+					if($letypeduchamp == "string" || $letypeduchamp == "blob")
+						$insertions .=  "'";
+							
+						$insertions .= addslashes($lavaleurduchamp);
+							
+						if($letypeduchamp == "string" ||$letypeduchamp == "blob")
+							$insertions .=  "'";
+			}
+	
+			$insertions .=  ");\n";
+	
+		}
+	
+		$insertions .= "\n";
+	
+	}	
+	
+	
+	if ($repertoire == NULL) 
+		$repertoire = ".";
+	
+	if ($nomfichier == NULL) {	
+		$info = date("dMy(H-i-s)");	
+	    $nomfichier = $dbname."_".$info.".sql";
+	}
+	
+	$nf =  $repertoire."/".$nomfichier;
+	
+	//echo "Nom fic : $nf <br />";
+	
+	$fichierDump = fopen($nf, "w");
+	fwrite($fichierDump, $entete);
+	//echo "Entete $dbname sauvegardee...<br />";
+	if ($mode == 'S' or $mode == 'A') {
+		fwrite($fichierDump, $creations);
+		//echo "Structure de la base $dbname sauvegardee...<br />";
+	}
+	if ($mode == 'D' or $mode == 'A') {
+		fwrite($fichierDump, $insertions);
+		//echo "Donnees de la base $dbname sauvegardee...<br />";
+	}
+	fclose($fichierDump);
+	
+}
+
+
+
+/**
+ *
+ *Exporte la structure et les donnees de la base de donnees courante à la racine
+ *avec pour nom le nom de la base de données.
+ *
+ */
+function dump() {
+	export("A",NULL,nomBase().".sql");
+}
+
+
+
+/**
+ *
+ *Exporte la structure de la base de donnees courante à la racine
+ *avec pour nom le nom de la base de données.
+ *
+ */
+function exportBase() {
+	export("S",NULL,nomBase().".sql");
+}
+
+
+
+/**
+ *Sauvegarde dans le répertoire 'repertoire' les données de la base.
+ *le nom du fichier est géré en fonction de la date et de l'heure
+ * @param repertoire string
+ *  <p>Nom du repertoire dans lequel le fichier dump sera sauvagarde
+ *     si NULL on sauvegarde dans le repertoire courant</p>
+ *
+ */
+function sauvegarde($repertoire) {
+	dump("D",$repertoire,NULL);
+}
+
+
+
+/**
+ *
+ *Restaure depuis un fichier texte la base de donnees courante.
+ *
+ ** @param repertoire string
+ *  <p>Nom du repertoire dans lequel le fichier dump sera sauvagarde
+ *     si NULL on sauvegarde dans le repertoire courant</p>
+ */
+function import(){
+	
+	global $modeacces, $connexion;
+	
+	//desactive les cle etrangere pour pouvoir effacer les tables de la base
+	$sql = "SET FOREIGN_KEY_CHECKS = 0";
+	echo "SQL : $sql<br/>";
+	$result = executeSQL($sql);
+	
+	//quelques informations importantes
+	$ipsrv=$_SERVER['SERVER_ADDR'];
+	$versionPHP=getenv("SERVER_SOFTWARE");
+	$namesrv=$_SERVER['SERVER_NAME'];
+	$dbname=nomBase();
+	
+	//si la base existe on supprime les tables
+	if ($connexion) {
+		$sql = "SHOW tables";
+		$results = tableSQL($sql);
+		foreach ($results as $ligne) {
+			//on extrait chaque valeur de la ligne courante
+			$nomTable = $ligne[0];
+	
+			$sql = "DROP TABLE `$nomTable`";
+			echo "SQL : $sql<br/>";
+			$result = executeSQL($sql);
+		}
+	}  else {
+		//sinon on cree la base
+		$sql="CREATE DATABASE `$dbname` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
+		echo "SQL : $sql<br/>";
+		$result = executeSQL($sql);
+	}
+	
+	//on utilise la base de donnees
+	$sql="USE `$dbname`";
+	echo "SQL : $sql<br/>";
+	$result = executeSQL($sql);
+	 
+	 
+	$versionMySQL=versionMYSQL();
+	
+	echo "Version : ".$versionMySQL;
+	
+	
+	
+	//restaure la base en fonction du fichier creer par le dump (V2)
+	//Lit le fichier et renvoie le résultat dans un tableau
+	$lines = file($dbname.".sql");
+	$versionBase=$lines[1];
+	
+	$sql="";
+	
+	//execute toutes les requetes du fichier de dump
+	for($i=0;$i<count($lines);$i++){
+		$line=$lines[$i];
+		if ($line[0]!='-' && $line[0]!='/' && strlen($line)>2) {
+			$sql=$sql.$line;
+			$test=strrev($line);
+			$pospv=strpos($test,";");
+			if ($pospv>0 && $pospv<5){
+				echo "SQL : $sql<br/>";
+				$result = executeSQL($sql);
+				$sql="";
+			}
+		}
+	}
+	
+	
+	//reactive les cle etrangere la base
+	$sql = "SET FOREIGN_KEY_CHECKS = 1";
+	echo "SQL : $sql<br/>";
+	$result = executeSQL($sql);
+	
+	$versionBase= substr($versionBase,23+strlen($dbname),strlen($versionBase)-23-strlen($dbname));
+	$versionBase = str_replace("à","<br />",$versionBase);
+	
+	echo "<font color=green> Version base  $dbname <br /> $versionBase <br />$namesrv(IP : $ipsrv)<br />$versionPHP<br />MySQL : $versionMySQL</font>";
+	
+	
+}
+
 
 
 
