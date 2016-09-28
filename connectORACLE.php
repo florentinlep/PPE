2c288f2d1ad21af0a32d6bc374216f2b9815ac87
erwanLeGuen <erwan.leguen2@free.fr>
2016-09-28 11:44:54 +0200

    V2

--- connectORACLE.php
+++ connectORACLE.php
@@ -1,14 +1,23 @@
 <?php
 
+echo "Liste des drivers PDO<br />";
+foreach(PDO::getAvailableDrivers() as $driver)
+	echo $driver,"<br />";
+
+	//$test = getenv('LD_LIBRARY_PATH')."  ".getenv('ORACLE_HOME');
+	
+echo "<hr />";	
+
+
 	//configurer le driver PDO Oracle dans le fichier php.ini
 	//     extension=php_pdo_oci.dll
 	
 	
-	$host='10.23.104.80';    	// le chemin vers le serveur
+	$host='10.0.220.100';    	// le chemin vers le serveur
 	$port='1521';				// 1521 port pour oracle 3306 pour mysql
-	$dbname='orcl';  	        // le nom de votre base de données ou du service oracle
-	$user='sdis';  			    // nom d'utilisateur pour se connecter
-	$password='Estr@n29';       // mot de passe de l'utilisateur
+	$dbname='ORAPROF';  	        // le nom de votre base de données ou du service oracle
+	$user='scott';  			    // nom d'utilisateur pour se connecter
+	$password='tiger';       // mot de passe de l'utilisateur
 
 	
 	// ceation du Data Source Name, ou DSN, qui contient les infos
@@ -33,15 +42,15 @@
 	{
 		echo 'Erreur : '.$e->getMessage().'<br />';
 		echo 'N° : '.$e->getCode();
-		die();
+		//die();
 	}	
 
-	echo "Connexion ouverte<br />";	
+	/*echo "Connexion ouverte<br />";	
 	
 	if ($connexion) {
 		
 		// on va chercher tous les infos de la table
-		$resultats=$connexion->query("SELECT * FROM GRADE");
+		$resultats=$connexion->query("SELECT * FROM EMP;");
 		
 		// on récupére le résultat sous forme d'objet
 		$resultats->setFetchMode(PDO::FETCH_OBJ);
@@ -50,13 +59,23 @@
 		while( $ligne = $resultats->fetch() )
 		{
 			// on affiche les infos
-			echo "Information : $ligne->GRA_LIBELLE <br />";
+			echo "Information : $ligne->ENAME <br />";
 		}
 		
 		$connexion = NULL ; // Fermeture de la connexion
 		echo "Connexion fermee";
 	}
-
+*/
+	phpinfo();
+	echo "<hr />";
+ 
+	$conn = oci_connect('scott', 'tiger', '10.0.220.100:1521/ORAPROF');
+	if (!$conn) {
+		$e = oci_error();
+		var_dump($e);
+		//trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+	
 
 ?>
 
